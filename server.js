6380016f42b0d95ce0918a9e3b049cad7cb7c6d7
dyashton <ashtonblainedy@gmail.com>
2026-02-24 17:24:48 -0500

    fixing modules

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // SPA routing for React Router
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
