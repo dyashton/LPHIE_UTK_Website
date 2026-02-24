9a07fc727b1ed4fe63d26c0b9f538192299d9367
dyashton <ashtonblainedy@gmail.com>
2026-02-24 18:34:33 -0500

    back to normal routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouterRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import BrothersProvider from './providers/BrothersProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
