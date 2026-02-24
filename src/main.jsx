a4028e228fbed0be2cdc026070baade37f74361f
dyashton <ashtonblainedy@gmail.com>
2026-02-24 18:32:33 -0500

    back to normal routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouterRouter } from 'react-router-dom'
 import BrothersProvider from './providers/BrothersProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <BrowserRouter>
     <BrothersProvider>
       <App />
     </BrothersProvider>
-  </HashRouter>
+  </BrowserRouter>
 )
