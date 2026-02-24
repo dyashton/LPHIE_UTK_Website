b26782965a4c24c6a2563ccae77e63b184a6330b
dyashton <ashtonblainedy@gmail.com>
2026-02-24 18:18:05 -0500

    lazy loading

--- src/pages/Brothers.jsx
+++ src/pages/Brothers.jsx
@@ -87,6 +87,7 @@ function BrotherCard({ brother, images, jumpToBrother, extendedBrother, setExten
                     </div>
                 </div>
                 <motion.img src={images[brother.lineName]} alt={brother.getFullName()} className="brother-image absolute top-0 right-0 w-[18rem] h-full object-cover"
+                    loading="lazy"
                     variants={fadeMask}
                     initial="hidden"
                     animate={extendedBrother === brother.lineName ? "visible" : "hidden"}
